d95176564d75828646915cd84fdfb3853b1ecd3b
Teddy <tainguyen197.ntt@gmail.com>
2025-05-23 13:18:59 +0700

    Refactor AboutPage component: Update service item structure to enhance layout consistency and improve accessibility by nesting service details within the link element.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -197,51 +197,52 @@ export default function AboutPage() {
         </div>
         <div className="container bg-[#FEF6EA] mx-auto px-4 max-w-6xl py-14 md:p-16 rounded-4xl">
           {services.map((service, index) => (
-            <div key={service.id} className="mb-10 last:mb-0">
-              <div className="overflow-hidden">
-                <div className="flex flex-col md:flex-row md:gap-x-10 md:pb-10 md:border-b border-[#E2E2E2] cursor-pointer">
-                  <Link
-                    href={`/services/${service.id}`}
-                    className="absolute inset-0"
-                  />
-                  <div className="h-52 aspect-270/200  relative rounded-2xl overflow-hidden">
-                    <Image
-                      src={service.image}
-                      alt={service.title}
-                      className="object-cover w-full"
-                      fill
-                    />
-                  </div>
-                  <div className="md:w-2/3 p-6 md:p-0 group">
-                    <div className="flex flex-col md:flex-row justify-between items-start mb-3">
-                      <h3 className="text-xl md:text-4xl font-medium text-[#222222] font-cormorant group-hover:text-[#B1873F] transition-all">
-                        {service.title}
-                      </h3>
-
-                      <button className="bg-[#B1873F0D] cursor-pointer border border-[#B1873F] py-1.5 px-5 rounded-full text-[#B1873F] text-sm font-medium flex items-center group-hover:bg-[rgba(177,135,63,0.1)] transition-all whitespace-nowrap mt-2 md:mt-0">
-                        Xem chi tiết
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-4 w-4 ml-1 transition-transform group-hover:translate-x-1"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={1.5}
-                            d="M14 5l7 7m0 0l-7 7m7-7H3"
-                          />
-                        </svg>
-                      </button>
+            <div key={service.id} className="relative mb-10 last:mb-0">
+              <Link
+                href={`/services/${service.id}`}
+                className="group mt-2 md:mt-0"
+              >
+                <div className="overflow-hidden">
+                  <div className="flex flex-col md:flex-row md:gap-x-10 md:pb-10 md:border-b border-[#E2E2E2] cursor-pointer">
+                    <div className="h-52 aspect-270/200  relative rounded-2xl overflow-hidden">
+                      <Image
+                        src={service.image}
+                        alt={service.title}
+                        className="object-cover w-full"
+                        fill
+                      />
                     </div>
-                    <p className="text-md md:text-lg text-[#909090] leading-relaxed">
-                      {service.description}
-                    </p>
+                    <div className="md:w-2/3 p-6 md:p-0 group">
+                      <div className="flex flex-col md:flex-row justify-between items-start mb-3">
+                        <h3 className="text-xl md:text-4xl font-medium text-[#222222] font-cormorant group-hover:text-[#B1873F] transition-all">
+                          {service.title}
+                        </h3>
+
+                        <button className="bg-[#B1873F0D] cursor-pointer border border-[#B1873F] py-1.5 px-5 rounded-full text-[#B1873F] text-sm font-medium flex items-center group-hover:bg-[rgba(177,135,63,0.1)] transition-all whitespace-nowrap mt-2 md:mt-0">
+                          Xem chi tiết
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-4 w-4 ml-1 transition-transform group-hover:translate-x-1"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={1.5}
+                              d="M14 5l7 7m0 0l-7 7m7-7H3"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+                      <p className="text-md md:text-lg text-[#909090] leading-relaxed">
+                        {service.description}
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
+              </Link>
             </div>
           ))}
         </div>
