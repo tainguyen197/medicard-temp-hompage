139ac93a428b6110acbe87e43460df9e26ba9a9a
Teddy <tainguyen197.ntt@gmail.com>
2025-05-23 11:59:31 +0700

    Refactor ServicesPage component: Rearrange structure of service items for improved layout consistency and readability, ensuring the service title and description are properly nested within the link element.

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -64,25 +64,26 @@ export default function ServicesPage() {
         <div className="container mx-auto px-4 max-w-6xl">
           {services.map((service, index) => (
             <div key={service.id} className="mb-10 last:mb-0">
-              <div className="overflow-hidden">
-                <div className="flex flex-col md:flex-row md:gap-x-10 md:pb-10 md:border-b border-[#E2E2E2]">
-                  <div className="h-52 aspect-270/200  relative rounded-2xl overflow-hidden">
-                    <Image
-                      src={service.image}
-                      alt={service.title}
-                      className="object-cover w-full"
-                      fill
-                    />
-                  </div>
-                  <div className="md:w-2/3 p-6 md:p-0">
-                    <div className="flex flex-col md:flex-row justify-between items-start mb-3">
-                      <h3 className="text-xl md:text-4xl font-medium text-[#222222] font-cormorant">
-                        {service.title}
-                      </h3>
-                      <Link
-                        href={`/services/${service.id}`}
-                        className="group mt-2 md:mt-0"
-                      >
+              <Link
+                href={`/services/${service.id}`}
+                className="group mt-2 md:mt-0"
+              >
+                <div className="overflow-hidden">
+                  <div className="flex flex-col md:flex-row md:gap-x-10 md:pb-10 md:border-b border-[#E2E2E2]">
+                    <div className="h-52 aspect-270/200  relative rounded-2xl overflow-hidden">
+                      <Image
+                        src={service.image}
+                        alt={service.title}
+                        className="object-cover w-full"
+                        fill
+                      />
+                    </div>
+                    <div className="md:w-2/3 p-6 md:p-0">
+                      <div className="flex flex-col md:flex-row justify-between items-start mb-3">
+                        <h3 className="text-xl md:text-4xl font-medium text-[#222222] font-cormorant">
+                          {service.title}
+                        </h3>
+
                         <button className="bg-[#B1873F0D] cursor-pointer border border-[#B1873F] py-1.5 px-5 rounded-full text-[#B1873F] text-sm font-medium flex items-center hover:bg-[rgba(177,135,63,0.1)] transition-all whitespace-nowrap">
                           Xem chi tiết
                           <svg
@@ -100,14 +101,14 @@ export default function ServicesPage() {
                             />
                           </svg>
                         </button>
-                      </Link>
+                      </div>
+                      <p className="text-md md:text-lg text-[#909090] leading-relaxed">
+                        {service.description}
+                      </p>
                     </div>
-                    <p className="text-md md:text-lg text-[#909090] leading-relaxed">
-                      {service.description}
-                    </p>
                   </div>
                 </div>
-              </div>
+              </Link>
             </div>
           ))}
         </div>
